d807737eb95df30c685db6720f121d7c4329f595
Mayank Mohit Agarwal <mayankmohitagarwal7@gmail.com>
2026-04-24 23:13:04 +0530

    Initialised Project with dependencies

---
+++ prisma/migrations/20260424171643_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "Workflow" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT 'Untitled Workflow',
+    "nodes" JSONB NOT NULL DEFAULT '[]',
+    "edges" JSONB NOT NULL DEFAULT '[]',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Workflow_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkflowRun" (
+    "id" TEXT NOT NULL,
+    "workflowId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "scope" TEXT NOT NULL,
+    "duration" DOUBLE PRECISION,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WorkflowRun_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NodeRun" (
+    "id" TEXT NOT NULL,
+    "workflowRunId" TEXT NOT NULL,
+    "nodeId" TEXT NOT NULL,
+    "nodeType" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "inputs" JSONB,
+    "outputs" JSONB,
+    "error" TEXT,
+    "duration" DOUBLE PRECISION,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "NodeRun_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "WorkflowRun" ADD CONSTRAINT "WorkflowRun_workflowId_fkey" FOREIGN KEY ("workflowId") REFERENCES "Workflow"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NodeRun" ADD CONSTRAINT "NodeRun_workflowRunId_fkey" FOREIGN KEY ("workflowRunId") REFERENCES "WorkflowRun"("id") ON DELETE CASCADE ON UPDATE CASCADE;
